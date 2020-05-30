cb157099ea6811f5c59a88956a1bcb07f97ecb31
st974 <st974@drexel.edu>
2020-05-30 17:14:19 -0400

    Updates

---
+++ precache-manifest.ad0942b8c8072c86e410c5797aee7801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9873c3858e355a234df46fcfbcb8d3bb",
+    "url": "/weather-moods/index.html"
+  },
+  {
+    "revision": "fa95aad86a421e7e109a",
+    "url": "/weather-moods/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdaf1ae1b56ef8409ee0",
+    "url": "/weather-moods/static/js/2.1f18e49b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-moods/static/js/2.1f18e49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa95aad86a421e7e109a",
+    "url": "/weather-moods/static/js/main.b04e4219.chunk.js"
+  },
+  {
+    "revision": "bae8de5add960f165aaa",
+    "url": "/weather-moods/static/js/runtime-main.120b113d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-moods/static/media/logo.5d5d9eef.svg"
+  }
+]);
